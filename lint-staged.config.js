cae022b24c46fa86c76356ece5b27200c9ab9cd8
Craftzcat <craftzcat.ss@gmail.com>
2022-08-22 23:16:18 -0700

    [SETUP]

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+const options = {
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  '**/*.(ts|tsx|js)': filenames => [
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`
+  ],
+  '**/*.(md|json)': filenames => `yarn prettier --write ${filenames.join(' ')}`
+};
+
+module.exports = options;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const options = {
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+/** @type {import('tailwindcss').Config} */
+module.exports = options;
