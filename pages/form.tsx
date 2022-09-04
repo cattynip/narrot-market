fff054eb4fed543f51f11975ded7fb523b647490
Seol So <96553411+craftz-cat@users.noreply.github.com>
2022-09-04 16:51:55 -0700

    7.5 Errors

--- pages/form.tsx
+++ pages/form.tsx
@@ -11,9 +11,15 @@ interface LoginForm {
 }
 
 const FormPractice: NextPage = () => {
-  const { register, handleSubmit } = useForm<LoginForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginForm>({
+    mode: 'onChange'
+  });
 
-  const onValid = (/* _: LoginForm */) => {
+  const onValid = (/* data: LoginForm */) => {
     console.log('onvalid');
   };
 
@@ -27,6 +33,7 @@ const FormPractice: NextPage = () => {
         <input
           placeholder="Username"
           type="text"
+          className={errors.username ? 'border-red-500' : ''}
           {...register('username', {
             required: {
               value: true,
@@ -45,16 +52,22 @@ const FormPractice: NextPage = () => {
         <input
           placeholder="Email"
           type="email"
+          className={errors.email ? 'border-red-500' : ''}
           {...register('email', {
             required: {
               value: true,
               message: 'Email is required'
+            },
+            validate: {
+              notGmail: value =>
+                !value.includes('@gmail.com') || "We don't accept @gmail.com"
             }
           })}
         />
         <input
           placeholder="Password"
           type="password"
+          className={errors.password ? 'border-red-500' : ''}
           {...register('password', {
             required: {
               value: true,
@@ -66,6 +79,9 @@ const FormPractice: NextPage = () => {
             }
           })}
         />
+        <p>{errors?.username?.message}</p>
+        <p>{errors?.email?.message}</p>
+        <p>{errors?.password?.message}</p>
         <BeautifulButton buttonText="Apply" />
       </form>
     </Layout>
