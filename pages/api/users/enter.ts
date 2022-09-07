24e8c16033b1de4fbb8c97b07cb0e80157b96fff
Seol So <96553411+craftz-cat@users.noreply.github.com>
2022-09-06 17:07:53 -0700

    9.2 Token Logic

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -6,20 +6,29 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
 
-  const user = await client.user.upsert({
-    where: {
-      ...payload
-    },
-    create: {
-      name: 'Anonymous',
-      ...payload
-    },
-    update: {}
+  const token = await client.token.create({
+    data: {
+      payload: '1234',
+      user: {
+        // connectOrCreate
+        // If user exists, connect the user and the token,
+        // If not, create a user using `create` field.
+        connectOrCreate: {
+          where: {
+            ...payload
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload
+          }
+        }
+      }
+    }
   });
 
-  console.log('Upsert User : ', user);
+  console.log(token);
 
-  res.status(200).json({ user });
+  res.status(200).json({ ok: true });
 };
 
 export default withHandler('POST', handler);
