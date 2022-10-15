63344f9ac4ad1d41f2633725dd0fcbcf73d6098a
cattynip <cattynip.cattynip@gmail.com>
2022-10-14 22:04:07 -0700

    Refactor Badge Components

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import CommunityItem from '@components/communityItem';
 import Layout from '@components/layout';
+import Badge from '@components/badge';
 
 const Community: NextPage = () => {
   return (
@@ -19,7 +20,8 @@ const Community: NextPage = () => {
             key={idx}
           />
         ))}
-        <button className="transition ease-in-out fixed bottom-16 right-10 bg-orange-400 rounded-full p-4 text-white shadow-lg hover:-translate-y-2 hover:rotate-180 hover:bg-orange-500">
+
+        <Badge href='/community/write/'>
           <svg
             className="w-6 h-6"
             fill="none"
@@ -34,8 +36,9 @@ const Community: NextPage = () => {
               d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
             ></path>
           </svg>
-        </button>
-      </div>
+ 
+        </Badge>
+     </div>
     </Layout>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import useUser from '@libs/client/useUser';
 import Link from 'next/link';
 import useSWR from 'swr';
 import { GetProductsResponse } from './api/products';
+import Badge from '@components/badge';
 
 // `index` means the default route of a router of its father.
 // All of the pages must have the type `NextPage` which Next.js provide for Typescript.
@@ -32,27 +33,25 @@ const Home: NextPage = props => {
           />
         ))}
 
-        <Link href="/products/upload">
-          <button className="transition ease-in-out fixed bottom-24 right-6 bg-orange-400 rounded-full p-4 text-white shadow-lg hover:-translate-y-2 hover:rotate-180 hover:bg-orange-500">
-            <svg
-              className="h-7 w-7"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              />
-            </svg>
-          </button>
-        </Link>
+        <Badge href='/products/upload'>
+          <svg
+            className="h-7 w-7"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+            />
+          </svg>
+        </Badge>
       </div>
-    </Layout>
+    </Layout >
   );
 };
 
