c81768425017a18fe86061c9b804bc30ee9afd83
cattynip <cattynip.cattynip@gmail.com>
2022-10-27 00:22:56 -0700

    14.5 Seeding

--- pages/api/auth/[...nextauth].ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from 'next-auth';
-// Import providers
-
-NextAuth({
-  // Add providers with some of configuration variables
-  providers: []
-});
-
-export default NextAuth;
---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+async function createStreams() {
+  [...Array.from(Array(500).keys())].forEach(async item => {
+    await client.stream.create({
+      data: {
+        name: String(item),
+        description: String(item) + 'description',
+        price: item,
+        user: {
+          connect: {
+            id: 1
+          }
+        }
+      }
+    });
+
+    console.log(`/stream/${item}`);
+  });
+}
+
+createStreams()
+  .catch(error => console.log(error))
+  .finally(() => client.$disconnect);
