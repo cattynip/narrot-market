3bfad7a1a23db3e69d459ef97e893b3f29f1b2a1
Cattynip <cattynip.cattynip@gmail.com>
2023-01-02 08:26:37 -0800

    ~3.1 - Finish setting up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
-function Home() {
-  return <h1>Hello World</h1>;
-}
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-blue-500">It works</h1>
+    </div>
+  );
+};
 
 export default Home;
